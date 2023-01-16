0c056d84a5f75a4d04de148395d4d97ff19bbc39
Delfin656 <juandelfin656@gmail.com>
2023-01-16 09:47:33 +0000

    Diagrama UML de star wars

--- src/models.py
+++ src/models.py
@@ -8,26 +8,66 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
+
+class User(Base):
+    __tablename__ = 'user'
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    name = Column(String(50), nullable=False)
+    last_name = Column(String(50), nullable=False)
+    email = Column(String(50), nullable=False)
+    password = Column(String(30), nullable=False)
+    user_name = Column(String(30), nullable=False)
+    gender = Column(String(30), nullable=False)
+    favorites = relationship("Favorites")
 
-class Address(Base):
-    __tablename__ = 'address'
+
+class Characters(Base):
+    __tablename__ = 'characters'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(50), nullable=False)
+    gender = Column(String(30), nullable=False)
+    skin_color = Column(String(50), nullable=False)
+    eye_color = Column(String(50), nullable=False)
+    hair_color = Column(String(50), nullable=False)
+    height = Column(String(30), nullable=False)
+    favorites = relationship("Favorites")
+
+
+class Planets(Base):
+    __tablename__ = 'planets'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(50), nullable=False)
+    climate = Column(String(30), nullable=False)
+    terrain = Column(String(30), nullable=False)
+    population = Column(String(50), nullable=False)
+    diameter = Column(String(50), nullable=False)
+    favorites = relationship("Favorites")
+
+
+class Vehicles(Base):
+    __tablename__ = 'vehicles'
+    id = Column(Integer, primary_key=True)
+    model = Column(String(50), nullable=False)
+    color = Column(String(30), nullable=False)
+    length = Column(Integer, nullable=False)
+    favorites = relationship("Favorites")
+
+
+class Favorites(Base):
+    __tablename__ = 'favorites'
     # Here we define columns for the table address.
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    user_id = Column(Integer, ForeignKey('user.id'), nullable=False)
+    characters_id = Column(Integer, ForeignKey('characters.id'), nullable=False)
+    planets_id = Column(Integer, ForeignKey('planets.id'), nullable=False)
+    vehicles_id = Column(Integer, ForeignKey('vehicles.id'), nullable=False)
 
     def to_dict(self):
         return {}
 
-## Draw from SQLAlchemy base
+
+# Draw from SQLAlchemy base
 render_er(Base, 'diagram.png')
